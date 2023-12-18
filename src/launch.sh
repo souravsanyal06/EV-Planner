684a9ba69ef0c60d1e6d4bc5e3e6a9f7da14eaea
Sourav Sanyal <33360350+souravsanyal06@users.noreply.github.com>
2023-12-18 10:07:43 -0500

    Update launch.sh

--- src/launch.sh
+++ src/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source /opt/ros/noetic/setup.bash
-source ~/EV-Planner/devel/setup.bash
+source ~/cv_ws/devel/setup.bash
 source ~/killross.sh
 roslaunch rotors_gazebo test.launch lockstep:=true &
 #rosrun keyboard keyboard &
